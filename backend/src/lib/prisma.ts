d23affbc8a17bc8d231061a4b3adcaf1cc14e945
unknown <cauanp.santos@yahoo.com.br>
2026-07-29 11:35:53 -0300

    Refact: prisma-> backend

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,7 +1,3 @@
-import { PrismaClient } from "@prisma/client/extension";
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+import { PrismaClient } from "../../../prisma/generated/client.js";
+const prisma = new PrismaClient();
+const users = await prisma.user.findMany();
